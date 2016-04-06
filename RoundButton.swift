cd541da7b5f0e05a8cfc2b7d521d60dc1b158776
francinirc <francinirc@gmail.com>
2016-04-05 22:54:57 -0300

    Creating ViewControllers

---
+++ RoundButton.swift
@@ -0,0 +1,40 @@
+//
+//  RoundButton.swift
+//  HackaPonto
+//
+//  Created by Francini Roberta de Carvalho on 4/5/16.
+//  Copyright © 2016 Francini Carvalho. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class RoundButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+        
+    @IBInspectable var borderColor: UIColor = UIColor.whiteColor() {
+        didSet {
+            layer.borderColor = borderColor.CGColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 0.5 * bounds.size.width
+        clipsToBounds = true
+    }
+
+}
